8f71239628f955de237f8be21489c3ca4f6a1f50
vishnu karthik <karthikl@thoughtworks.com>
2014-05-30 18:40:07 +0530

    Adding spec file formatter

---
+++ src/com/thoughtworks/gauge/formatter/SpecFormatter.java
@@ -0,0 +1,50 @@
+package com.thoughtworks.gauge.formatter;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+
+import java.io.File;
+
+public class SpecFormatter extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        final Project project = anActionEvent.getData(LangDataKeys.PROJECT);
+        if (project == null) {
+            return;
+        }
+        String projectDir = project.getBasePath();
+        if (projectDir == null) {
+            return;
+        }
+
+        FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+        VirtualFile selectedFile = fileEditorManager.getSelectedFiles()[0];
+        String fileName = selectedFile.getCanonicalPath();
+        Document doc = FileDocumentManager.getInstance().getDocument(selectedFile);
+        if (doc != null) {
+            FileDocumentManager.getInstance().saveDocument(doc);
+        }
+
+        ProcessBuilder processBuilder = new ProcessBuilder("gauge", "--format", fileName);
+        processBuilder.directory(new File(projectDir));
+        try {
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                VirtualFileManager.getInstance().syncRefresh();
+                selectedFile.refresh(false, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Messages.showErrorDialog("Error on formatting spec","Format Error");
+        }
+    }
+}
