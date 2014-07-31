b6439f540d8ac750f1fa656bc65f25bd9ea0ba64
vishnu karthik <karthikl@thoughtworks.com>
2014-07-31 15:27:31 +0530

    Templating the args while insertion and removing intellij specific templates

--- src/com/thoughtworks/gauge/autocomplete/StepCompletionProvider.java
+++ src/com/thoughtworks/gauge/autocomplete/StepCompletionProvider.java
@@ -8,42 +8,44 @@ import com.intellij.codeInsight.template.TemplateBuilderFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
-import com.thoughtworks.gauge.StepValue;
-import com.thoughtworks.gauge.core.GaugeService;
-import com.thoughtworks.gauge.core.Gauge;
 import com.thoughtworks.gauge.GaugeConnection;
+import com.thoughtworks.gauge.StepValue;
+import com.thoughtworks.gauge.core.Gauge;
+import com.thoughtworks.gauge.core.GaugeService;
+import com.thoughtworks.gauge.language.psi.SpecArg;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StepCompletionProvider extends CompletionProvider<CompletionParameters> {
 
     public void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet resultSet) {
+        resultSet.stopHere();
         resultSet = resultSet.withPrefixMatcher(new PlainPrefixMatcher(""));
         for (String step : getStepsInProject(parameters.getOriginalFile().getProject())) {
             LookupElementBuilder element = LookupElementBuilder.create(step);
             element = element.withInsertHandler(new InsertHandler<LookupElement>() {
                 @Override
                 public void handleInsert(InsertionContext context, LookupElement item) {
-                    PsiElement stepElement = context.getFile().findElementAt(context.getStartOffset());
+                    PsiElement stepElement = context.getFile().findElementAt(context.getStartOffset()).getParent();
                     final TemplateBuilder builder = TemplateBuilderFactory.getInstance().createTemplateBuilder(stepElement);
-                    Pattern pattern = Pattern.compile("\\{\\}");
-                    if (stepElement != null) {
-                        Matcher matcher = pattern.matcher(stepElement.getText());
-                        while (matcher.find()) {
-                            builder.replaceRange(new TextRange(matcher.start(), matcher.end()), "\"arg\"");
-                        }
-                        builder.run(context.getEditor(), false);
+                    List<SpecArg> args = PsiTreeUtil.getChildrenOfTypeAsList(stepElement, SpecArg.class);
+                    for (SpecArg arg : args) {
+                        builder.replaceRange(getRangeWithinElement(arg), arg.getText());
                     }
+                    builder.run(context.getEditor(), false);
                 }
             });
             resultSet.addElement(element);
         }
+    }
+
+    private TextRange getRangeWithinElement(SpecArg arg) {
+        return new TextRange(arg.getStartOffsetInParent(), arg.getStartOffsetInParent() + arg.getTextLength());
     }
 
     private List<String> getStepsInProject(Project project) {
