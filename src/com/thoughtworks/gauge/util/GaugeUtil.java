8a462f07143b721d7278cf6dc9c5ab31d2532178
vishnu karthik <karthikl@thoughtworks.com>
2014-08-19 18:22:02 +0530

    Finding gauge from PATH by checking gauge executable's existence

--- src/com/thoughtworks/gauge/util/GaugeUtil.java
+++ src/com/thoughtworks/gauge/util/GaugeUtil.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.gauge.util;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.EnvironmentUtil;
 import com.thoughtworks.gauge.GaugeConstant;
 import org.apache.commons.lang.StringUtils;
@@ -7,9 +8,13 @@ import org.apache.commons.lang.StringUtils;
 import java.io.File;
 
 public class GaugeUtil {
+    private static final Logger LOG = Logger.getInstance("#com.thoughtworks.gauge.GaugeUtil");
+
     public static String getGaugeExecPath() {
         String path = EnvironmentUtil.getValue("PATH");
+        LOG.info("PATH => " + path);
         String gaugeHome = EnvironmentUtil.getValue("GAUGE_ROOT");
+        LOG.info("GAUGE_ROOT => " + gaugeHome);
         if (!StringUtils.isEmpty(gaugeHome)) {
             File bin = new File(gaugeHome, "bin");
             File gaugeExec = new File(bin, GaugeConstant.GAUGE);
@@ -18,10 +23,9 @@ public class GaugeUtil {
             }
         } else if (!StringUtils.isEmpty(path)) {
             for (String entry : path.split(File.pathSeparator)) {
-                if (entry.contains(GaugeConstant.GAUGE)) {
-                    if (new File(entry, GaugeConstant.GAUGE).exists()) {
-                        return new File(entry, GaugeConstant.GAUGE).getAbsolutePath();
-                    }
+                File gaugeExec = new File(entry, GaugeConstant.GAUGE);
+                if (gaugeExec.exists()) {
+                    return gaugeExec.getAbsolutePath();
                 }
             }
         }
