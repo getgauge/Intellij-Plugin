0015d387f2255016c8588e7e15b437e3557d83f7
vishnu karthik <vishnucarbon@gmail.com>
2014-09-08 10:48:56 +0530

    Autocompletion wasn't refreshing since intellij wasn't saving files periodically while editing. Intellij saves files only on some events. To force save, introduced a enter handler

---
+++ src/com/thoughtworks/gauge/GaugeEnterHandler.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.gauge;
+
+import com.intellij.codeInsight.editorActions.EnterHandler;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+
+public class GaugeEnterHandler extends EnterHandler {
+    public GaugeEnterHandler(EditorActionHandler originalHandler) {
+        super(originalHandler);
+    }
+
+    @Override
+    public void executeWriteAction(Editor editor, Caret caret, DataContext dataContext) {
+        super.executeWriteAction(editor, caret, dataContext);
+        FileDocumentManager.getInstance().saveDocument(editor.getDocument());
+    }
+}
