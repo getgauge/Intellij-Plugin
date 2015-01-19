01808d6c2a84e7c10cd57a7fd4b8c6a79686a1a4
prateekbaheti <prateektheone@gmail.com>
2015-01-19 12:11:34 +0530

    Refreshing files after refactoring

--- src/com/thoughtworks/gauge/rename/CustomRenameHandler.java
+++ src/com/thoughtworks/gauge/rename/CustomRenameHandler.java
@@ -1,23 +1,16 @@
 package com.thoughtworks.gauge.rename;
 
-import com.apple.eawt.Application;
 import com.intellij.codeInsight.hint.HintManager;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.RunResult;
-import com.intellij.openapi.application.impl.ApplicationImpl;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.command.impl.CurrentEditorProvider;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.InputValidator;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.newvfs.RefreshQueue;
-import com.intellij.psi.ExternalChangeAction;
-import com.intellij.psi.ExternalChangeAction.ExternalDocumentChange;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.refactoring.rename.RenameHandler;
@@ -26,19 +19,18 @@ import com.thoughtworks.gauge.language.psi.impl.SpecStepImpl;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
+import java.util.Arrays;
 
 import static com.thoughtworks.gauge.util.GaugeUtil.getGaugeExecPath;
 
 public class CustomRenameHandler implements RenameHandler {
 
     private PsiElement psiElement;
-    private Editor editor;
 
     public boolean isAvailableOnDataContext(DataContext dataContext) {
         PsiElement element = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
         if (element == null) {
             Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-            this.editor = editor;
             if (editor == null) return false;
             int offset = editor.getCaretModel().getOffset();
             if (offset > 0 && offset == editor.getDocument().getTextLength()) offset--;
@@ -69,7 +61,7 @@ public class CustomRenameHandler implements RenameHandler {
             }
         } else if (element.getClass().equals(SpecStepImpl.class))
             text = element.getText().replaceFirst("\\*", "").trim();
-        Messages.showInputDialog(project, String.format("Refactoring \"%s\" to : ", text), "Refactor", Messages.getInformationIcon(), text, new RenameInputValidator(project, this.editor, text));
+        Messages.showInputDialog(project, String.format("Refactoring \"%s\" to : ", text), "Refactor", Messages.getInformationIcon(), text, new RenameInputValidator(project, editor, text));
     }
 
     public void invoke(@NotNull Project project, @NotNull PsiElement[] elements, DataContext dataContext) {
@@ -110,23 +102,28 @@ public class CustomRenameHandler implements RenameHandler {
                 @Override
                 public void run() {
                     try {
+                        FileDocumentManager.getInstance().saveDocumentAsIs(editor.getDocument());
                         Process process = processBuilder.start();
-                        if (process.waitFor() != 0) {
-                            String errorMessage = "";
-                            errorMessage = getMessages(errorMessage, process.getInputStream());
-                            int flags = HintManager.HIDE_BY_ANY_KEY | HintManager.HIDE_BY_TEXT_CHANGE | HintManager.HIDE_BY_SCROLLING;
-                            int timeout = 0;
-                            HintManager.getInstance().showErrorHint(editor, errorMessage,
-                                    editor.getCaretModel().getOffset(), editor.getCaretModel().getOffset() + 1,
-                                    HintManager.ABOVE, flags, timeout);
-                        }
-                        LocalFileSystem.getInstance().refresh(false);
+                        String message = "";
+                        message = getMessages(message, process.getInputStream());
+                        refreshAllFiles();
+                        showMessage(message);
                     } catch (Exception e) {
                         Messages.showInfoMessage(String.format("Could not execute refactor command: %s", e.getMessage()), "Warning");
                     }
                 }
             });
             return true;
+        }
+
+        private void refreshAllFiles() {
+            VirtualFile currentFile = FileDocumentManager.getInstance().getFile(editor.getDocument());
+            LocalFileSystem.getInstance().refreshFiles(Arrays.asList(new VirtualFile[]{currentFile}));
+            LocalFileSystem.getInstance().refresh(false);
+        }
+
+        private void showMessage(String errorMessage) {
+            HintManager.getInstance().showErrorHint(editor, errorMessage);
         }
 
         private static String getMessages(String errorMessage, InputStream stream) throws IOException {
