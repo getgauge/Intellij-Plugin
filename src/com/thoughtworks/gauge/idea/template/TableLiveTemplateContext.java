a6b673b633908aa5849f9b06e331c5de90c0f0be
ApoorvaM <apoorva_rarefriend@yahoo.com>
2015-04-23 15:38:31 +0530

    Setting context of table to gauge file

--- src/com/thoughtworks/gauge/idea/template/TableLiveTemplateContext.java
+++ src/com/thoughtworks/gauge/idea/template/TableLiveTemplateContext.java
@@ -2,16 +2,18 @@ package com.thoughtworks.gauge.idea.template;
 
 import com.intellij.codeInsight.template.TemplateContextType;
 import com.intellij.psi.PsiFile;
+import com.thoughtworks.gauge.language.ConceptFileType;
+import com.thoughtworks.gauge.language.SpecFileType;
 import com.thoughtworks.gauge.util.GaugeUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class TableLiveTemplateContext extends TemplateContextType {
-    protected TableLiveTemplateContext(String id, String presentableName) {
-        super(id, presentableName);
+    protected TableLiveTemplateContext() {
+        super("GAUGE", "Gauge");
     }
 
     @Override
     public boolean isInContext(@NotNull PsiFile psiFile, int i) {
-        return GaugeUtil.isGaugeFile(psiFile.getVirtualFile());
+        return psiFile.getFileType() instanceof SpecFileType || psiFile.getFileType() instanceof ConceptFileType;
     }
 }
