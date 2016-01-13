5855505b0c906dec6ddcf94395beb4e0d8838d5f
apoorvam <apoorva_rarefriend@yahoo.com>
2016-01-13 15:38:27 +0530

    updating github_release, with proper artifact name

--- github_release.sh
+++ github_release.sh
@@ -9,6 +9,6 @@ version=$(ls Gauge-Java-Intellij-*.zip | sed "s/^Gauge-Java-Intellij-\([^;]*\).z
 
 $GOPATH/bin/github-release release -u getgauge -r "Intellij-Plugin" --draft -t "v$version" -d "## New Features: ## Enhancements: ## Bug Fixes:" -n "Gauge-Java-Intellij $version"
 
-for i in `ls *.zip`; do
-    $GOPATH/bin/github-release -v upload -u getgauge -r "Intellij-Plugin" -t "v$version" -n $i -f $i
-done
+cd artifacts
+
+$GOPATH/bin/github-release -v upload -u getgauge -r "Intellij-Plugin" -t "v$version" -n "Gauge Intellij Plugin $version" -f "Gauge-Java-Intellij-$version.zip"
