fdc3b617fd2467ca33ebdb2125709021183ee239
Kashishm <kashishmunjal64@gmail.com>
2016-09-01 16:30:15 +0530

    Checking for Gauge module before showing run scenario option

--- src/com/thoughtworks/gauge/execution/TestRunLineMarkerProvider.java
+++ src/com/thoughtworks/gauge/execution/TestRunLineMarkerProvider.java
@@ -7,6 +7,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.util.Function;
+import com.thoughtworks.gauge.helper.ModuleHelper;
 import com.thoughtworks.gauge.language.token.SpecTokenTypes;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,10 +17,20 @@ import java.util.List;
 
 public class TestRunLineMarkerProvider extends RunLineMarkerContributor {
     private static final Function<PsiElement, String> TOOLTIP_PROVIDER = psiElement -> "Run Element";
+    private ModuleHelper helper;
+
+    public TestRunLineMarkerProvider(ModuleHelper helper) {
+        this.helper = helper;
+    }
+
+    public TestRunLineMarkerProvider() {
+        this.helper = new ModuleHelper();
+    }
 
     @Nullable
     @Override
     public Info getInfo(PsiElement psiElement) {
+        if (!this.helper.isGaugeModule(psiElement)) return null;
         List<IElementType> types = Arrays.asList(SpecTokenTypes.SPEC_HEADING, SpecTokenTypes.SCENARIO_HEADING);
         if (psiElement instanceof LeafPsiElement && types.contains(((LeafPsiElement) psiElement).getElementType()))
             return new Info(AllIcons.RunConfigurations.TestState.Run, TOOLTIP_PROVIDER, ExecutorAction.getActions(1));
