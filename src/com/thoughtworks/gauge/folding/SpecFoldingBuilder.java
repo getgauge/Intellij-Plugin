66808fc38a0104970cb5a7b0ea17f5be829ac6dd
Kashishm <kashishmunjal64@gmail.com>
2016-10-21 19:59:47 +0530

    Adding expand/collapse feature for context & teardown

--- src/com/thoughtworks/gauge/folding/SpecFoldingBuilder.java
+++ src/com/thoughtworks/gauge/folding/SpecFoldingBuilder.java
@@ -3,6 +3,7 @@ package com.thoughtworks.gauge.folding;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
 import com.thoughtworks.gauge.language.token.SpecTokenTypes;
 import org.jetbrains.annotations.NotNull;
@@ -15,8 +16,14 @@ public class SpecFoldingBuilder extends GaugeFoldingBuilder {
     @Override
     public FoldingDescriptor[] buildFoldRegions(@NotNull ASTNode astNode, @NotNull Document document) {
         List<FoldingDescriptor> descriptors = new ArrayList<>();
-        for (ASTNode node : astNode.getChildren(TokenSet.create(SpecTokenTypes.SCENARIO)))
-            addNode(descriptors, node, node.findChildByType(SpecTokenTypes.SCENARIO_HEADING));
+        addNodes(astNode, descriptors, SpecTokenTypes.SPEC_DETAIL, SpecTokenTypes.SPEC_HEADING);
+        addNodes(astNode, descriptors, SpecTokenTypes.SCENARIO, SpecTokenTypes.SCENARIO_HEADING);
+        addNodes(astNode, descriptors, SpecTokenTypes.TEARDOWN, SpecTokenTypes.TEARDOWN_IDENTIFIER);
         return descriptors.toArray(new FoldingDescriptor[0]);
+    }
+
+    private void addNodes(@NotNull ASTNode astNode, List<FoldingDescriptor> descriptors, IElementType pNode, IElementType cNode) {
+        for (ASTNode node : astNode.getChildren(TokenSet.create(pNode)))
+            addNode(descriptors, node, node.findChildByType(cNode));
     }
 }
