d0dccbf711eb8615eb8e3f4a730b3a9ec5a7aa9c
Kashishm <kashishmunjal64@gmail.com>
2016-12-23 13:55:05 +0530

    Adding implemented steps to autocomplete list #122

--- src/com/thoughtworks/gauge/autocomplete/StepCompletionProvider.java
+++ src/com/thoughtworks/gauge/autocomplete/StepCompletionProvider.java
@@ -26,9 +26,9 @@ import com.intellij.codeInsight.template.TemplateBuilderFactory;
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
-import com.thoughtworks.gauge.ConceptInfo;
 import com.thoughtworks.gauge.GaugeConnection;
 import com.thoughtworks.gauge.StepValue;
 import com.thoughtworks.gauge.core.Gauge;
@@ -36,15 +36,17 @@ import com.thoughtworks.gauge.core.GaugeService;
 import com.thoughtworks.gauge.language.psi.ConceptArg;
 import com.thoughtworks.gauge.language.psi.SpecArg;
 import com.thoughtworks.gauge.util.GaugeUtil;
+import com.thoughtworks.gauge.util.StepUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.thoughtworks.gauge.autocomplete.StepCompletionContributor.getPrefix;
+import static com.thoughtworks.gauge.language.psi.SpecPsiImplUtil.getStepValueFor;
+import static com.thoughtworks.gauge.util.StepUtil.getGaugeStepAnnotationValues;
 
 public class StepCompletionProvider extends CompletionProvider<CompletionParameters> {
 
@@ -110,7 +112,7 @@ public class StepCompletionProvider extends CompletionProvider<CompletionParamet
         private String text;
         private String type;
 
-        public Type(String text, String type) {
+        Type(String text, String type) {
             this.text = text;
             this.type = type;
         }
@@ -124,28 +126,35 @@ public class StepCompletionProvider extends CompletionProvider<CompletionParamet
         }
     }
 
-    private List<Type> getStepsInModule(Module module) {
-        List<Type> steps = new ArrayList<>();
+    private Collection<Type> getStepsInModule(Module module) {
+        Map<String, Type> steps = getImplementedSteps(module);
         try {
             GaugeService gaugeService = Gauge.getGaugeService(module, true);
-            if (gaugeService == null)
-                return steps;
+            if (gaugeService == null) return steps.values();
             GaugeConnection gaugeConnection = gaugeService.getGaugeConnection();
             if (gaugeConnection != null) {
-                for (StepValue stepValue : gaugeConnection.fetchAllSteps())
-                    addStep(steps, stepValue);
-                for (ConceptInfo conceptInfo : gaugeConnection.fetchAllConcepts())
-                    addStep(steps, conceptInfo.getStepValue());
-                return steps;
+                gaugeConnection.fetchAllSteps().forEach(s -> addStep(steps, s, STEP));
+                gaugeConnection.fetchAllConcepts().forEach(concept -> addStep(steps, concept.getStepValue(), CONCEPT));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ignored) {
         }
+        return steps.values();
+    }
+
+    @NotNull
+    private Map<String, Type> getImplementedSteps(Module module) {
+        Map<String, Type> steps = new HashMap<>();
+        Collection<PsiMethod> methods = StepUtil.getStepMethods(module);
+        methods.forEach(m -> {
+            getGaugeStepAnnotationValues(m).forEach(s -> {
+                steps.put(getStepValueFor(module, m, s, false).getStepText(), new Type(s, STEP));
+            });
+        });
         return steps;
     }
 
-    private void addStep(List<Type> steps, StepValue stepValue) {
-        if (stepValue.getStepAnnotationText().trim().isEmpty()) return;
-        steps.add(new Type(stepValue.getStepAnnotationText(), STEP));
+    private void addStep(Map<String, Type> steps, StepValue stepValue, String entity) {
+        if (stepValue.getStepAnnotationText().trim().isEmpty() || steps.containsKey(stepValue.getStepText())) return;
+        steps.put(stepValue.getStepText(), new Type(stepValue.getStepAnnotationText(), entity));
     }
 }
