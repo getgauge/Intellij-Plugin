48504d605afa8f2e604ce9081b1fd0b89e1df96a
BugDiver <vinaysh@thoughtworks.com>
2017-09-04 15:59:33 +0530

    Showing line no on step failure.

--- src/com/thoughtworks/gauge/execution/runner/event/ExecutionError.java
+++ src/com/thoughtworks/gauge/execution/runner/event/ExecutionError.java
@@ -3,14 +3,20 @@ package com.thoughtworks.gauge.execution.runner.event;
 public class ExecutionError {
     public String text;
     public String filename;
+    public String lineNo;
     public String message;
     public String stackTrace;
 
     public String format() {
         return format(this.text, "Failed: ", "\n") +
-                format(this.filename, "Filename: ", "\n") +
+                format(getFileNameWithLineNo(), "Filename: ", "\n") +
                 format(this.message, "Message: ", "\n") +
                 format(this.stackTrace, "Stack Trace:\n", "");
+    }
+
+    private String getFileNameWithLineNo() {
+        return lineNo.isEmpty() ? filename : format(":", filename, lineNo);
+
     }
 
     private String format(String text, String prefix, String suffix) {
