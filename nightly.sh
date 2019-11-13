b39358dd220dac90035dcd87fc761503b4a1b621
Nehashri <nehashri@in-Nehazhrl.local>
2019-11-13 12:51:57 +0530

    refactoring github release script. Removing release from gradle intellij plugin and nightly release script

--- nightly.sh
+++
@@ -1,3 +0,0 @@
-version=$(ls Gauge-Java-Intellij-*.zip | sed "s/^Gauge-Java-Intellij-\([^;]*\).zip/\1/")
-
-curl -k -i --request POST -F userName="$username" -F password="$password" -F channel="$channel" -F pluginId=7535 -F file="@Gauge-Java-Intellij-$version.zip" https://plugins.jetbrains.com/plugin/uploadPlugin
